1521ceb63a05c1a23026b69a56a08831f0a90cc6
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-25 09:02:11 -0300

    Update portugol.py

--- portugol.py
+++ portugol.py
@@ -11,6 +11,71 @@ programa {
     senao{
      escreva ("O salário vale: ",salario)
     }
+  }
+}
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+programa {
+  funcao inicio() {
+    inteiro intervalo
+    escreva("Insira o número: ")
+    leia(intervalo)
+
+    se (intervalo <= 50 e intervalo >= 10){
+      escreva("Está no intervalo")
+    }
+    senao{
+      escreva("Não está no intervalo")
+    }
+  }
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+programa {
+  funcao inicio() {
+    logico ingresso
+    inteiro idade
+
+    escreva("Insira a idade: ")
+    leia(idade)
+
+    escreva("Você possui um inggresso? ")
+    leia(ingresso)
+
+    se (ingresso == "sim"){
+      ingresso == verdadeiro
+    }
+    senao{
+      ingresso == falso
+    }
+
+    se (idade >= 18 e ingresso){
+      escreva("Você pode entrar no evento")
+    }
+    senao{
+      escreva("Acesso negado")
+    }
+  }
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+programa {
+  funcao inicio() {
+    cadeia autenticacao, cadastro
+    escreva("cadastre a sua senha: ")
+    leia(cadastro)
+
+    escreva("Faça o seu login: ")
+    leia(autenticacao)
+
+    se (cadastro == autenticacao){
+      escreva("Senha correta!")
+    }
+    senao{
+      escreva("A senha está incorreta!")
+    }
   }
 }
